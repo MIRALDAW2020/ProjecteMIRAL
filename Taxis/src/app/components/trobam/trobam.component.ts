08edc25a2a0636664c9f51cea1d1d67f8943fc9a
MarcB98 <marcbatalla652@gmail.com>
2021-04-08 16:58:09 +0200

    Ok

--- Taxis/src/app/components/trobam/trobam.component.ts
+++ Taxis/src/app/components/trobam/trobam.component.ts
@@ -6,54 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trobam.component.css']
 })
 export class TrobamComponent implements OnInit {
-  // lat : Number =40;
-  lat :number;
-  lng :number;
-  zoom:number;
-  mapTypeId: string;
-  constructor() {
 
-    // latitud del mapa
-    this.lat =40.6123,
-     // // longitud
-    this.lng =40.6123,
-      // // zoomb  del mapa 
-    this.zoom=10;
-    // // tipo de vista de mapa 
-    this.mapTypeId="hybrid";
-
-  }
-  getCurrentPosition(){
-    
-    navigator.geolocation.getCurrentPosition(position =>{
-
-      this.lat = position.coords.latitude;
-      this.lng = position.coords.longitude;
-      this.zoom=14;
-    })
-
-    
-    // navigator.geolocation.watchPosition;
-
-  }
-
-// asda
-  ngOnInit(): void {
-    navigator.geolocation.getCurrentPosition(position =>{
-      
-      this.lat = position.coords.latitude;
-      this.lng = position.coords.longitude;
-      
-
-    })
-  }
-
-
-  
-
-
-
-
-
-
+  constructor() {}
+  ngOnInit(): void {}
 }
