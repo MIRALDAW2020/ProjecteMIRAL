c2f9122d6d14bb73da2d8ae6945dc3e540b9aea0
MarcB98 <marcbatalla652@gmail.com>
2021-04-08 16:35:29 +0200

    Contacte reactive

--- Taxis/src/app/components/contacte/contacte.component.ts
+++ Taxis/src/app/components/contacte/contacte.component.ts
@@ -10,28 +10,28 @@ export class ContacteComponent implements OnInit {
 
   user!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-
-    
-   }
+  constructor(private formBuilder: FormBuilder) {}
 
   submitted = false;
 
   ngOnInit(): void {
 
     // Condicions dels camps
-    // this.user = this.formBuilder.group({
+    this.user = this.formBuilder.group({
 
-    //   nom: ['', [Validators.required, Validators.minLength(2),Validators.maxLength(20),  Validators.pattern('[a-zA-Z]*[^[\s]{1}[a-zA-Z]*]?')]],
-    //   cognom: ['', [Validators.required, Validators.minLength(2),Validators.maxLength(50),  Validators.pattern('[a-zA-Z]*[^[\s]{1}[a-zA-Z]*]?')]]
-      // nickname: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(15), Validators.pattern('[\Sa-zA-Z_0-9\S]*')]],
-      // correo: ['', [Validators.required, Validators.email, Validators.maxLength(50), Validators.minLength(7)]],
-      // password: ['', [Validators.required, Validators.minLength(9),Validators.maxLength(20), Validators.pattern('[\Sa-zA-Z_0-9\S]*')]],
-      // password1: ['', [Validators.required, Validators.minLength(9),Validators.maxLength(20), Validators.pattern('[\Sa-zA-Z_0-9\S]*')]],
-      // centro: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(20),  Validators.pattern('[a-zA-Z]*[^[\s]{1}[a-zA-Z]*]?')]]
+      nom: ['', [Validators.required, Validators.minLength(2),Validators.maxLength(20),  Validators.pattern('[a-zA-Z]*[^[\s]{1}[a-zA-Z]*]?')]],
+      cognom: ['', [Validators.required, Validators.minLength(2),Validators.maxLength(50),  Validators.pattern('[a-zA-Z]*[^[\s]{1}[a-zA-Z]*]?')]],
+      nickname: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(15), Validators.pattern('[\Sa-zA-Z_0-9\S]*')]],
+      correo: ['', [Validators.required, Validators.email, Validators.maxLength(50), Validators.minLength(7)]],
+      password: ['', [Validators.required, Validators.minLength(9),Validators.maxLength(20), Validators.pattern('[\Sa-zA-Z_0-9\S]*')]],
+      password1: ['', [Validators.required, Validators.minLength(9),Validators.maxLength(20), Validators.pattern('[\Sa-zA-Z_0-9\S]*')]],
+      centro: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(20),  Validators.pattern('[a-zA-Z]*[^[\s]{1}[a-zA-Z]*]?')]]
 
-    // })
+    })
 
+  }
+  get f(){
+    return this.user.controls;
   }
 
   enviarDatos(){
