4ff68faf4b25b335a5dad7fa6ff05f5fda222018
Marc Batalla Tapia <57674196+MarcB98@users.noreply.github.com>
2021-04-28 16:34:07 +0200

    Perfil Usuari v1

--- Taxis/src/app/components/perfil/perfil.component.ts
+++ Taxis/src/app/components/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  show: boolean;
+  perfilForm: FormGroup;
+
+  validation_messages = {
+    fname: [
+      { type: 'required', message: 'El campo nombre es obligatorio' },
+      { type: 'minlength', message: 'El campo nombre debe contener como mínimo 3 carácteres' },
+    ],
+    lname: [
+      { type: 'required', message: 'El campo apellidos es obligatorio' },
+      { type: 'minlength', message: 'El campo apellidos debe contener como mínimo 3 carácteres' },
+    ],
+    email: [
+      { type: 'required', message: 'El campo e-mail es obligatorio' },
+      { type: 'minlength', message: 'El campo e-mail debe contener como mínimo 5 carácteres' },
+      { type: 'email', message: 'El campo e-mail no tiene buen formato' },
+    ],
+    phone: [
+      { type: 'required', message: 'El campo telefono es obligatorio' },
+      { type: 'minlength', message: 'El campo contraseña debe contener como mínimo 9 números' },
+    ],
+    password: [
+      { type: 'required', message: 'El campo contraseña es obligatorio' },
+      { type: 'minlength', message: 'El campo contraseña debe contener como mínimo 6 carácteres' },
+    ],
+  };
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  )
+  {
+    this.show = false;
+    this.perfilForm = this.formBuilder.group({
+      fname: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(3)]),
+      lname: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(3)]),
+      email: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(5), Validators.email]),
+      phone: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(9)]),
+      password: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(6)])
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  password() {
+    this.show = !this.show;
+  }
+
+  saveUser(){
+    this.perfilForm.get('fname')?.disable();
+    this.perfilForm.get('lname')?.disable();
+    this.perfilForm.get('email')?.disable();
+    this.perfilForm.get('password')?.disable();
+    this.perfilForm.get('phone')?.disable();
+  }
+
+  editarInfo(){
+    this.perfilForm.get('fname')?.enable();
+    this.perfilForm.get('lname')?.enable();
+    this.perfilForm.get('email')?.enable();
+    this.perfilForm.get('password')?.enable();
+    this.perfilForm.get('phone')?.enable();
   }
 
 }
