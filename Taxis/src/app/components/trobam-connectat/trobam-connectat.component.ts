0fb8d492c592e1a87896bdd7d924bd26a3f99128
Cacike69 <stlucano@yahoo.es>
2021-05-14 21:25:45 +0200

    Intento cojer datos del formulario

--- Taxis/src/app/components/trobam-connectat/trobam-connectat.component.ts
+++ Taxis/src/app/components/trobam-connectat/trobam-connectat.component.ts
@@ -1,55 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { Reserve } from 'src/app/models/reserva.models';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-trobam-connectat',
   templateUrl: './trobam-connectat.component.html',
-  styleUrls: ['./trobam-connectat.component.css']
+  styleUrls: ['./trobam-connectat.component.css'],
 })
 export class TrobamConnectatComponent implements OnInit {
+  public reservaHtml!: FormGroup;
+  public submited = false;
 
-  formContact!: FormGroup;
-  reserva!: Reserve
-  UsuarioService: any;
+  public nuevaReserva!: Reserve;
+  public UsuarioService: any;
 
-  reservaHtml : FormGroup;
+  _correo = '';
 
-  constructor(
-
-
-    formBuilder: FormBuilder
-  ) {
-
-    this.reservaHtml = new FormGroup({
-
-    });
-
-  }
+  constructor(private formBuilder: FormBuilder) {}
 
   submitted = false;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-     //creamos las condiciones de los campos del formulario de registro
-
-
-
+  //sirve para ejecutar el control del formulario en el html
+  get f() {
+    return this.formBuilder.controls;
   }
 
+  formularioReserva() {
+    console.log(this._correo);
 
-  formularioReserva(){
-    this.reserva=new Reserve();
+    console.log('Funcione');
+    this.submitted = true;
+    if (this.formBuilder.invalid) {
+      return;
+    }
 
+    this.nuevaReserva = new Reserve(
+      this.formBuilder.controls.correoFormulario.value
+    );
+    //  console.log(this.missatge);
 
-
+    //  this.contacteService.enviarMissatge(this.missatge).subscribe((datos:any)=>{
+    //    if (datos['resultado'] == 'OK') {
+    //      let $mensaje = datos['mensaje'];
+    //      Swal.fire({
+    //        position: 'center',
+    //        icon: 'success',
+    //        title: 'Perfecte',
+    //        text: $mensaje,
+    //        showConfirmButton: false,
+    //        timer: 1500,
+    //      });
+    //    }else{
+    //      Swal.fire({
+    //        icon: 'error',
+    //        title: 'Oops...',
+    //        text: 'Ha sorgit un error inesperat!'
+    //      })
+    //    }
+    //  });
   }
-
-
-
-
-
 
   // enviarDatos(){
   //   console.log("Funcione");
@@ -85,8 +102,4 @@ export class TrobamConnectatComponent implements OnInit {
   //   });
 
   // }
-
-
-
-
 }
